7519458d5609956a6d066f53e2f5d57c8260f90a
Sebastian Siza Correa <s.siza10@uniandes.edu.co>
2025-09-19 11:11:32 -0500

    Ajuste branch

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -4,7 +4,7 @@ from Comunidad.persona import Persona
 
 
 class PersonaTestCase(unittest.TestCase):
-
+# dsd
    def setUp(self):
       self.persona1 = Persona(nombre='Alejandra', edad=25)
       self.persona2 = Persona(nombre='Diego', edad=22)
